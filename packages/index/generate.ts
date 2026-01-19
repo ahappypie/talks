4aa9f86adad4c747a0d88f807e603da9c8f90ad6
Brian Bagdasarian <bbagdasarian.scs@gmail.com>
2026-01-19 15:05:53 -0800

    fix index

--- packages/index/generate.ts
+++ packages/index/generate.ts
@@ -3,7 +3,7 @@ import { readdir, writeFile } from 'node:fs/promises'
 async function main() {
   const packages = await readdir('../')
 
-  const presentations = packages.filter(p => p !== 'index')
+  const presentations = packages.filter(p => p !== 'index' && p !== '.DS_Store')
   await writeFile('./index.json', JSON.stringify(presentations))
 }
 
